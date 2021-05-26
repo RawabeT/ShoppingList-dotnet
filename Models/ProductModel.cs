68bf25189faca3f95dcc2be7d645467e582d1e6d
rawabe4 <rawabe44u@gmail.com>
2021-05-26 20:27:33 +0300

    Add project files.

---
+++ Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shooping.Models
+{
+    public class ProductModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
+    }
+}
